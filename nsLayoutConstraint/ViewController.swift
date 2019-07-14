34a7e17c688cb7357ee900348a3018d61f4c1e59
mac <mac@shiva--R.local>
2019-07-14 20:37:01 +0530

    first commit

---
+++ nsLayoutConstraint/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  nsLayoutConstraint
+//
+//  Created by mac on 7/10/19.
+//  Copyright © 2019 shiva+R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,uita{
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+      
+    
+    }
+
+
+}
+
---
+++ nsLayoutConstraint/addTaskViewController.swift
@@ -0,0 +1,26 @@
+//
+//  addTaskViewController.swift
+//  nsLayoutConstraint
+//
+//  Created by mac on 7/10/19.
+//  Copyright © 2019 shiva+R. All rights reserved.
+//
+
+import UIKit
+
+class addTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func saveAction(_ sender: Any) {
+        
+    }
+    
+    
+
+}
